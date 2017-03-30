5b20b43da337c9cb35e53b626488edbbdf0e73bf
Sergey Kalinovsky <lord.skiminok@gmail.com>
2017-03-30 19:22:32 +0300

    make shine

--- Entities/src/main/java/com/criticalgnome/blog/entities/Record.java
+++ Entities/src/main/java/com/criticalgnome/blog/entities/Record.java
@@ -19,7 +19,6 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = false)
 @Entity
 @Table(name = "records")
 public class Record extends Pojo implements Serializable {
@@ -46,4 +45,34 @@ public class Record extends Pojo implements Serializable {
             inverseJoinColumns = {@JoinColumn(name = "tag_id")}
             )
     private Set<Tag> tags = new HashSet<Tag>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Record record = (Record) o;
+
+        if (id != null ? !id.equals(record.id) : record.id != null) return false;
+        if (header != null ? !header.equals(record.header) : record.header != null) return false;
+        if (body != null ? !body.equals(record.body) : record.body != null) return false;
+        if (createdAt != null ? !createdAt.equals(record.createdAt) : record.createdAt != null) return false;
+        if (modifiedAt != null ? !modifiedAt.equals(record.modifiedAt) : record.modifiedAt != null) return false;
+        if (category != null ? !category.equals(record.category) : record.category != null) return false;
+        return author != null ? author.equals(record.author) : record.author == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + (header != null ? header.hashCode() : 0);
+        result = 31 * result + (body != null ? body.hashCode() : 0);
+        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+        result = 31 * result + (modifiedAt != null ? modifiedAt.hashCode() : 0);
+        result = 31 * result + (category != null ? category.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        return result;
+    }
 }
--- Services/src/main/java/com/criticalgnome/blog/services/Service.java
+++ Services/src/main/java/com/criticalgnome/blog/services/Service.java
@@ -14,10 +14,8 @@ import java.util.List;
  */
 public interface Service<T extends Pojo> {
 
-    void create(T entity) throws DAOException, ServiceException;
-    List<T> getAll() throws DAOException, ServiceException;
-    T getById(int id) throws DAOException, ServiceException;
-    void update(T entity) throws DAOException, ServiceException;
-    void remove(int id) throws DAOException, ServiceException;
-    int getMaxId() throws DAOException, ServiceException;
+    void create(T pojo) throws DAOException, ServiceException;
+    T getById(Long id) throws DAOException, ServiceException;
+    void update(T pojo) throws DAOException, ServiceException;
+    void remove(Long id) throws DAOException, ServiceException;
 }
