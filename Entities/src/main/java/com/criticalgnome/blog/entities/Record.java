87a16c4849a5fc61999cdcd48ca5e796393d0c18
Sergey Kalinovsky <lord.skiminok@gmail.com>
2017-04-10 21:03:25 +0300

    Make shine

--- Entities/src/main/java/com/criticalgnome/blog/entities/Record.java
+++ Entities/src/main/java/com/criticalgnome/blog/entities/Record.java
@@ -5,9 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.GregorianCalendar;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
